4262a32ebb705371e6991e651cf05ce8014cbe83
CPradeep1995 <75578527+CPradeep1995@users.noreply.github.com>
2020-12-10 18:12:12 +0530

    Delete BookItem.py

--- BookItem.py
+++
@@ -1,8 +0,0 @@
-class BookItem:
-    def __init__(self,book,isbn,rack):
-        self.book = book
-        self.isbn = isbn
-        self.rack = rack
-
-    def __repr__(self):
-        return self.isbn + ' ' + self.rack
